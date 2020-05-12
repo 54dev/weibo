f0ba32def85f730d0389f10b20b3c1127f93e699
xiaoke li <1001code@gmail.com>
2020-05-12 22:34:31 +0000

    permissions system

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,6 +7,14 @@ use Auth;
 
 class SessionController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +29,8 @@ class SessionController extends Controller
 
         if(Auth::attempt($credentials, $request->has('remember'))){
             session()->flash('success','welcome back');
-            return redirect()->route('users.show',[Auth::user()]);
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
         }else{
             session()->flash('danger','sorry,no empty');
             return redirect()->back()->withInput();
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use function foo\func;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        //修改策略自动发现的逻辑
+        Gate::guessPolicyNamesUsing(function ($modelClass){
+            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        });
 
         //
     }
