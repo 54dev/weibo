4354f036e5e1daa1d3a388a90ef8cb320513881e
xiaoke li <1001code@gmail.com>
2020-05-13 01:07:52 +0000

    user statuses list

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(StatusesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
 
         Model::reguard();
